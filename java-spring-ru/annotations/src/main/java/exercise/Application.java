510a1874996c2766ed04015a1945ae681884fad4
@hexlet/cli <support@hexlet.io>
2024-05-22 21:38:36 +0200

    submit java-spring-ru/annotations

--- java-spring-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-ru/annotations/src/main/java/exercise/Application.java
@@ -12,7 +12,6 @@ public class Application {
 
             if (method.isAnnotationPresent(Inspect.class)) {
                 System.out.println("Method " + method.getName() + "returns a value of type " + method.getReturnType());
-                System.out.println("Execution time: " + executionTime + " milliseconds");
             }
         }
     }
