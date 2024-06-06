f521ef8068be675d2c382187b892f83a2610d90e
@hexlet/cli <support@hexlet.io>
2024-06-06 23:09:43 +0200

    submit java-spring-ru/jpa-comparing-entities

---
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
@@ -0,0 +1,29 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+//@AllArgsConstructor
+@Getter
+@Setter
+//@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @EqualsAndHashCode.Include
+    private String title;
+
+    @EqualsAndHashCode.Include
+    private Double price;
+}
---
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import exercise.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitleAndPrice(String title, Double price);
+}
