5caf8b99344cf879ffbb8bc997a30682e4e810a3
@hexlet/cli <support@hexlet.io>
2024-06-08 20:28:49 +0200

    submit java-spring-ru/jpa-repository

---
+++ java-spring-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,49 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
+import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProductsByPriceRange(
+            @RequestParam(required = false) Integer min,
+            @RequestParam(required = false) Integer max) {
+
+        if (min == null) {
+            min = 0;
+        }
+        if (max == null) {
+            max = Integer.MAX_VALUE;
+        }
+
+        List<Product> products = productRepository.findByPriceBetween(min, max);
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Product show(@PathVariable long id) {
+
+        var product =  productRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        return product;
+    }
+}
---
+++ java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import exercise.model.Product;
+
+import org.springframework.data.domain.Sort;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByPriceBetween(Integer minPrice, Integer maxPrice);
+}
