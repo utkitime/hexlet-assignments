ccc9de2b52c194862b4810365100ba0bb4cabd0a
@hexlet/cli <support@hexlet.io>
2024-06-08 10:09:07 +0200

    submit java-spring-ru/jpa-comparing-entities

--- java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
@@ -1,14 +1,8 @@
 package exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -32,6 +26,7 @@ public class ProductsController {
     }
 
     @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
     public Product createProduct(@RequestBody Product productData) {
         var existingProduct = productRepository.findByTitleAndPrice(
                 productData.getTitle(), productData.getPrice()
