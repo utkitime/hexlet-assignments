78deec07375be5fee07de4f2bd0c9bb5710feba7
@hexlet/cli <support@hexlet.io>
2024-06-23 22:48:03 +0200

    submit java-spring-ru/authentication

--- java-spring-ru/authentication/src/main/java/exercise/model/User.java
+++ java-spring-ru/authentication/src/main/java/exercise/model/User.java
@@ -53,6 +53,41 @@ public class User implements BaseEntity, UserDetails {
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
     private List<Article> articles = new ArrayList<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
     // BEGIN
     
     // END
